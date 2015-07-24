7f8c64b3015432b25148d605d3b68f13156ef44c
balboni <brian@brianbalboni.com>
2015-07-24 16:32:27 -0500

    M:M Project_Category relationship complete

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,6 @@ class Project < ActiveRecord::Base
 
 	belongs_to :user
 	has_many :activities
+	has_and_belongs_to_many :categories
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719192920) do
+ActiveRecord::Schema.define(version: 20150724212917) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "subject"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20150719192920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "project_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_projects", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "project_id",  null: false
   end
 
   create_table "profiles", force: :cascade do |t|
