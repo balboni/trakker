3e3792ec5aa93382532938e74fc2e522ff836a7d
balboni <brian@brianbalboni.com>
2015-08-07 12:17:44 -0500

    added users views and index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
 	has_one :profile
-	has_many :projects, -> { order('created_at DESC, name ASC')}, dependent => destroy
+	has_many :projects, -> { order('created_at DESC, name ASC')}
 
 end
